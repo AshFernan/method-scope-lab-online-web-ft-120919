d2dd3ae3c16baefa7e81bf52425663b37e0a4a67
Asha Fernandez <ash.fernan90@gmail.com>
2019-12-11 23:19:00 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,7 @@
-def rescue_princess_peach(status)
+def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
 end
 
-rescue_princess_peach(status)
+rescue_princess_peach
 puts status
